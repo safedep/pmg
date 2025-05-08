1dd07f13bb2672c6ba2f1a396d2ab70b4b5ddfe4
Sahilb315 <bansalsahil315@gmail.com>
2025-05-09 03:05:50 +0530

    replace AnalysePackage with QueryPackageAnalysis API & use community API

--- cmd/npm/npm.go
+++ cmd/npm/npm.go
@@ -24,10 +24,6 @@ func NewNpmCommand() *cobra.Command {
 			}
 
 			if len(args) >= 2 && utils.IsInstallCommand(string(registry.RegistryNPM), args[0]) {
-				if err := utils.ValidateEnvVars(); err != nil {
-					return err
-				}
-
 				pmw := wrapper.NewPackageManagerWrapper(registry.RegistryNPM)
 				pmw.Action = args[0]
 				pmw.PackageName = args[1]
