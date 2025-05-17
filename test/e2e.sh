bb69c9dc220b79dcdde29deba3564b6c770c455f
abhisek <abhisek.datta@gmail.com>
2025-05-17 19:13:26 +0530

    test: fix E2E scripts

--- test/e2e.sh
+++ test/e2e.sh
@@ -3,17 +3,14 @@
 set -e
 
 scriptDir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-
-if ! command -v pmg &> /dev/null; then
-    alias pmg="$scriptDir/../bin/pmg"
-fi
+pmg=$scriptDir/../bin/pmg
 
 echo "Running e2e tests..."
 
 ## All these should be successful
-pmg npm install express --dry-run
-pmg --dry-run npm install express
-pmg --dry-run pnpm add express
+$pmg npm install express --dry-run
+$pmg --dry-run npm install express
+$pmg --dry-run pnpm add express
 
 ## All these should fail
-pmg --dry-run npm install nyc-config@10.0.0
+$pmg --dry-run npm install nyc-config@10.0.0
