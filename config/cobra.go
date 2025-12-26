1757cfbaf62ad9be2e564e3ea504d8dab872d368
abhisek <abhisek.datta@gmail.com>
2025-12-26 16:22:55 +0530

    fix: Config cobra command should override and not replace

--- config/cobra.go
+++ config/cobra.go
@@ -4,13 +4,17 @@ import "github.com/spf13/cobra"
 
 // ApplyCobraFlags applies the cobra flags to the command.
 // These flags are local concern of the config package. This helper function is used
-// to bind them to the Cobra command.
+// to bind them to the Cobra. The default values are taken from the global configuration,
+// allowing for overriding the configuration at runtime.
 func ApplyCobraFlags(cmd *cobra.Command) {
-	cmd.PersistentFlags().BoolVar(&globalConfig.Config.Transitive, "transitive", true, "Resolve transitive dependencies")
-	cmd.PersistentFlags().IntVar(&globalConfig.Config.TransitiveDepth, "transitive-depth", 5,
-		"Maximum depth of transitive dependencies to resolve")
-	cmd.PersistentFlags().BoolVar(&globalConfig.Config.IncludeDevDependencies, "include-dev-dependencies", false,
-		"Include dev dependencies in the dependency graph (slows down resolution)")
-	cmd.PersistentFlags().BoolVar(&globalConfig.DryRun, "dry-run", false, "Dry run skips execution of package manager")
-	cmd.PersistentFlags().BoolVar(&globalConfig.Config.Paranoid, "paranoid", false, "Perform active scanning of unknown packages (slow)")
+	cmd.PersistentFlags().BoolVar(&globalConfig.Config.Transitive, "transitive",
+		globalConfig.Config.Transitive, "Resolve transitive dependencies")
+	cmd.PersistentFlags().IntVar(&globalConfig.Config.TransitiveDepth, "transitive-depth",
+		globalConfig.Config.TransitiveDepth, "Maximum depth of transitive dependencies to resolve")
+	cmd.PersistentFlags().BoolVar(&globalConfig.Config.IncludeDevDependencies, "include-dev-dependencies",
+		globalConfig.Config.IncludeDevDependencies, "Include dev dependencies in the dependency graph (slows down resolution)")
+	cmd.PersistentFlags().BoolVar(&globalConfig.DryRun, "dry-run",
+		globalConfig.DryRun, "Dry run skips execution of package manager")
+	cmd.PersistentFlags().BoolVar(&globalConfig.Config.Paranoid, "paranoid",
+		globalConfig.Config.Paranoid, "Perform active scanning of unknown packages (slow)")
 }
