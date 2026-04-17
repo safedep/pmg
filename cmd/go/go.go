40f0dbd38455b4aa6ded32ec2215eff92b50e7c5
atharvamhaske <atharvamhaske76@gmail.com>
2026-04-17 09:45:37 +0530

    feat: add go command parser

--- cmd/go/go.go
+++ cmd/go/go.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/safedep/pmg/internal/analytics"
 	"github.com/safedep/pmg/internal/ui"
+	"github.com/safedep/pmg/packagemanager"
 	"github.com/spf13/cobra"
 )
 
@@ -30,7 +31,17 @@ func NewGoCommand() *cobra.Command {
 func executeGoFlow(ctx context.Context, args []string) error {
 	analytics.TrackCommandGo()
 
-	cmd := exec.CommandContext(ctx, "go", args...)
+	packageManager, err := packagemanager.NewGoPackageManager(packagemanager.DefaultGoPackageManagerConfig())
+	if err != nil {
+		return fmt.Errorf("failed to create go package manager: %w", err)
+	}
+
+	parsedCommand, err := packageManager.ParseCommand(args)
+	if err != nil {
+		return fmt.Errorf("failed to parse command: %w", err)
+	}
+
+	cmd := exec.CommandContext(ctx, parsedCommand.Command.Exe, parsedCommand.Command.Args...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
